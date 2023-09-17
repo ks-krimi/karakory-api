187f3324b1267f391a365be91c87fd1ec8562310
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-09-17 16:30:39 +0300

    feat: transform auth app to hybride instance

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,31 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const USER = configService.get<string>('RABBITMQ_USER');
+  const PASSWORD = configService.get<string>('RABBITMQ_PASS');
+  const HOST = configService.get<string>('RABBITMQ_HOST');
+  const PORT = configService.get<number>('RABBITMQ_PORT');
+  const AUTH_QUEUE = configService.get<string>('RABBITMQ_AUTH_QUEUE');
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${USER}:${PASSWORD}@${HOST}:${PORT}`],
+      queue: AUTH_QUEUE,
+      noAck: false,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+  app.startAllMicroservices();
+  app.listen(3001);
 }
 bootstrap();
