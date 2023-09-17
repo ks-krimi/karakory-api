278a8370be75f951f08d8eb94429ab844e164e94
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-09-17 16:31:51 +0300

    feat: subscribe to incoming message with get_user pattern

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,4 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+
 import { AuthService } from './auth.service';
 
 @Controller()
@@ -8,5 +15,20 @@ export class AuthController {
   @Get()
   getHello(): string {
     return this.authService.getHello();
+  }
+
+  @MessagePattern({ cmd: 'get_user' })
+  async getUser(
+    @Payload() data: { userId: number },
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+    channel.ack(message);
+    console.log(
+      `Received message with payload ${JSON.stringify(data)}`,
+      'Auth Service',
+    );
+    return { service: 'Auth Service', params: data.userId };
   }
 }
