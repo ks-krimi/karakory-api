c079413a9f1fda0e86cb7cdaf046b4146b242f95
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-09-20 07:03:45 +0300

    feat: connecte to database via typeorm module and use user entity

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
 @Module({
   imports: [
@@ -14,12 +16,30 @@ import { AuthService } from './auth.service';
         RABBITMQ_HOST: Joi.string().required(),
         RABBITMQ_PORT: Joi.number().required(),
         RABBITMQ_AUTH_QUEUE: Joi.string().required(),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
       }),
       ignoreEnvVars: true, // fix: validation of predefined env variables
       validationOptions: {
         allowUnknown: false,
         abortEarly: true,
       },
+    }),
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        database: configService.get('POSTGRES_DB'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: true, // used only for development purposes
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
