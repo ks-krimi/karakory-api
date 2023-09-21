a08789632cfa8446f6bd451eb1969f3f670a604b
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-09-21 07:39:16 +0300

    refactor(api): use shared rabbitmq module
    register to auth service and presence service

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -3,7 +3,10 @@ import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('users')
 export class AppController {
-  constructor(@Inject('AUTH_SERVICE') private authService: ClientProxy) {}
+  constructor(
+    @Inject('AUTH_SERVICE') private authService: ClientProxy,
+    @Inject('PRESENCE_SERVICE') private presenceService: ClientProxy,
+  ) {}
 
   async onApplicationBootstrap() {
     try {
@@ -11,6 +14,11 @@ export class AppController {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  @Get('presence')
+  async getPresence() {
+    return this.presenceService.send({ cmd: 'get_presence' }, {});
   }
 
   @Get(':id')
--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,47 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 
+import { SharedModule } from '@app/shared';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    /* ClientsModule.register([
-      {
-        name: 'AUTH_SERVICE',
-        transport: Transport.RMQ,
-      },
-    ]), */
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    SharedModule.register('AUTH_SERVICE', process.env.RABBITMQ_AUTH_QUEUE),
+    SharedModule.register(
+      'PRESENCE_SERVICE',
+      process.env.RABBITMQ_PRESENCE_QUEUE,
+    ),
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    /*
-     * instead of hard coding the connection to the microservice,
-     * we can use the ConfigService to get the values from the .env file
-     */
-    {
-      provide: 'AUTH_SERVICE',
-      useFactory: (configService: ConfigService) => {
-        const USER = configService.get<string>('RABBITMQ_USER');
-        const PASSWORD = configService.get<string>('RABBITMQ_PASS');
-        const HOST = configService.get<string>('RABBITMQ_HOST');
-        const PORT = configService.get<number>('RABBITMQ_PORT');
-        const AUTH_QUEUE = configService.get<string>('RABBITMQ_AUTH_QUEUE');
-        return ClientProxyFactory.create({
-          transport: Transport.RMQ,
-          options: {
-            urls: [`amqp://${USER}:${PASSWORD}@${HOST}:${PORT}`],
-            queue: AUTH_QUEUE,
-          },
-        });
-      },
-      inject: [ConfigService],
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
